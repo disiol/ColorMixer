52951e245478c4eb4c2cc2ef1dd24bc9a76e229b
disiol <denisimus_games@ukr.net>
2022-07-15 00:24:16 +0300

    fix

---
+++ Assets/ColorMixer/Scripts/Game/Resources/Strings.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ColorMixer.Scripts.Game.Resources
+{
+    public static class Strings
+    {
+        public static String Victory => "Победа !!!";
+        public static String TryAgain => "Пробуйте еще раз";
+
+        public static String TheObtainedColorCorrespondsToTheRequiredColorBy =>
+            "{0} \n Полученный цвет на {1}% соответствует необходимому";
+    }
+}
